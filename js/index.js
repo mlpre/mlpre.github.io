bb539d4d5ff3cf986abac8d3e93f64bc939576cf
mlpre <43497290+mlpre@users.noreply.github.com>
2025-12-25 19:47:05 +0800

    Update and rename ml-index.js to index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,13 @@
 const translations = {
     zh: {
         title: '闵立的个人网站',
-        ml_title: '闵立的个人网站',
-        ml_open: '开源项目',
-        ml_blog: '我的博客',
+        open: '开源项目',
+        blog: '我的博客',
         language: 'EN'
     }, en: {
         title: 'Min Li\'s Personal Website',
-        ml_title: 'Min Li\'s Personal Website',
-        ml_open: 'Open Source Projects',
-        ml_blog: 'My Blog',
+        open: 'Open Source Projects',
+        blog: 'My Blog',
         language: 'ZH'
     }
 };
@@ -29,9 +27,9 @@ function switchLanguage() {
 function loadLanguage(language) {
     const content = translations[language];
     document.title = content.title;
-    document.getElementById('ml-title').innerText = content.ml_title;
-    document.getElementById('ml-open').querySelector('span').innerText = content.ml_open;
-    document.getElementById('ml-blog').querySelector('span').innerText = content.ml_blog;
+    document.getElementById('title').innerText = content.title;
+    document.getElementById('open').querySelector('span').innerText = content.open;
+    document.getElementById('blog').querySelector('span').innerText = content.blog;
     document.getElementById('language').innerText = content.language;
     document.body.classList.add('loaded');
 }
